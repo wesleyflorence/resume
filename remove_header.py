cbb3f74b150903f5d350692e86dda9aaf5f87419
Wesley Florence <wesleyflorence@Wesleys-Mac-Studio.local>
2023-11-11 15:06:41 -0800

    Using Spans not Divs

--- remove_header.py
+++ remove_header.py
@@ -8,29 +8,36 @@ def action(elem, doc):
         if 'Email :' in content and 'Mobile :' in content:
             return []  # Return an empty list to remove this element
 
-        # This function will be used to wrap text in Div and convert to Para for proper HTML structure
-        def wrap_in_div(text, class_name):
-            return pf.Div(pf.Para(pf.Str(text)), classes=[class_name])
+        # This function will create a Para that contains two Spans for organization and location
+        def create_combined_para(organization_text, location_text):
+            organization_span = pf.Span(pf.Str(organization_text), classes=['resume-organization'])
+            location_span = pf.Span(pf.Str(location_text), classes=['resume-location'])
+            return pf.Para(organization_span, pf.Space(), location_span)
 
         divs = []
         for row in elem.content:
             if len(row.content) >= 2:
                 cells = row.content
 
+                # Extract organization and location text
                 organization = pf.stringify(cells[0].content[0]).strip()
                 location = pf.stringify(cells[0].content[1]).strip()
-                organization_div = wrap_in_div(organization, 'resume-organization')
-                location_div = wrap_in_div(location, 'resume-location')
+                
+                # Create a paragraph with organization and location spans
+                combined_para = create_combined_para(organization, location)
 
                 sub_divs = []
                 for subcell in cells[1:]:
                     title = pf.stringify(subcell.content[0]).strip()
                     timeframe = pf.stringify(subcell.content[1]).strip()
-                    title_div = wrap_in_div(title, 'resume-title')
-                    timeframe_div = wrap_in_div(timeframe, 'resume-timeframe')
-                    sub_divs.append(title_div)
-                    sub_divs.append(timeframe_div)
-                entry_div = pf.Div(organization_div, location_div, *sub_divs, classes=['resume-header-entry'])
+                    title_span = pf.Span(pf.Str(title), classes=['resume-title'])
+                    timeframe_span = pf.Span(pf.Str(timeframe), classes=['resume-timeframe'])
+                    
+                    # Add title and timeframe spans to a list, separated by space for proper formatting
+                    sub_divs.append(pf.Para(title_span, pf.Space(), timeframe_span))
+
+                # Create a div for the entry and include the combined organization and location paragraph
+                entry_div = pf.Div(combined_para, *sub_divs, classes=['resume-header-entry'])
                 divs.append(entry_div)
 
         return divs
